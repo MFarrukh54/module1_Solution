d7c4354dbedd0e4888df899c53d16312871d4822
MFarrukh54 <farrukh_54@hotmail.com>
2018-10-27 09:34:29 +0500

    Update the app name

--- App.js
+++ App.js
@@ -1,1 +1,36 @@
-!function(){function e(e){e.list="",e.Value="",e.showResult=function(){var l=function(e){var l="";if(""==e)l="Please enter data first";else{var n=e.split(",");l=n.length>0&&n.length<=3?"Enjoy!":"Too much!"}return l}(e.list);e.value=l},e.clearValue=function(){e.value=""}}angular.module("Module1App",[]).controller("Module1Controller",e),e.$inject=["$scope"]}();
+(function () {
+    
+ angular.module('LunchCheck',[])
+
+ .controller('LunchCheckController',LunchCheckController);
+
+ LunchCheckController.$inject=['$scope'];
+
+function LunchCheckController($scope){
+    $scope.list="";
+    $scope.Value="";
+    $scope.showResult=function(){
+         var result=getResult($scope.list);
+        $scope.value=result;
+    }
+    $scope.clearValue=function(){
+        $scope.value="";
+    }
+}
+function getResult(string){
+var result=""
+if(string==""){
+    result="Please enter data first";
+}
+else{     
+var listValue=string.split(',');
+if(listValue.length>0 && listValue.length<=3){
+    result="Enjoy!";
+}
+else  {
+    result="Too much!";
+}
+}
+return result;
+}
+})();
